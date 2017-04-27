54d6216415b89a0190d9639da54d4ae2a3e57381
sysleg <zlou.sysleg@gmail.com>
2017-04-27 21:44:20 +0300

    Старт приложения PhotoGallery

---
+++ app/src/main/java/com/bignerdranch/android/photogallery/PhotogGalleryFragment.java
@@ -0,0 +1,36 @@
+package com.bignerdranch.android.photogallery;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Dmitry on 27.04.2017.
+ */
+
+public class PhotogGalleryFragment extends Fragment {
+    private RecyclerView mPhotoRecyclerView;
+
+    public static PhotogGalleryFragment newInstance(){
+        return new PhotogGalleryFragment();
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setRetainInstance(true);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_photo_gallery, container, false);
+        mPhotoRecyclerView = (RecyclerView) view.findViewById(R.id.fragment_photo_gallery_recycler_view);
+        mPhotoRecyclerView.setLayoutManager(new GridLayoutManager(getActivity(),3));
+        return view;
+    }
+}

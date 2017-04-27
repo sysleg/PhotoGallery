52e7cc322da5f53cb3d8fa7d3d884628daa4dd24
sysleg <zlou.sysleg@gmail.com>
2017-04-27 22:04:07 +0300

    Используется AsyncTask

--- app/src/main/java/com/bignerdranch/android/photogallery/PhotogGalleryFragment.java
+++ app/src/main/java/com/bignerdranch/android/photogallery/PhotogGalleryFragment.java
@@ -1,13 +1,17 @@
 package com.bignerdranch.android.photogallery;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import java.io.IOException;
 
 /**
  * Created by Dmitry on 27.04.2017.
@@ -15,6 +19,7 @@ import android.view.ViewGroup;
 
 public class PhotogGalleryFragment extends Fragment {
     private RecyclerView mPhotoRecyclerView;
+    private static final String TAG = "PhotoGalleryFragment";
 
     public static PhotogGalleryFragment newInstance(){
         return new PhotogGalleryFragment();
@@ -24,6 +29,7 @@ public class PhotogGalleryFragment extends Fragment {
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setRetainInstance(true);
+        new FetchItemsTask().execute();
     }
 
     @Override
@@ -32,5 +38,20 @@ public class PhotogGalleryFragment extends Fragment {
         mPhotoRecyclerView = (RecyclerView) view.findViewById(R.id.fragment_photo_gallery_recycler_view);
         mPhotoRecyclerView.setLayoutManager(new GridLayoutManager(getActivity(),3));
         return view;
+    }
+
+    private class FetchItemsTask extends AsyncTask<Void,Void,Void>{
+
+
+        @Override
+        protected Void doInBackground(Void... params) {
+            try{
+                String result = new FlickrFetchr().getUrlString("https://www.bignerranch.com");
+            }catch (IOException ioe)
+            {
+                Log.e(TAG, "Failed to fetch URL: " ,ioe);
+            }
+            return null;
+        }
     }
 }

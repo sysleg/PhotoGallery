3ae24551fa145e8a94661ecb2a2fe16bee75fe67
sysleg <zlou.sysleg@gmail.com>
2017-05-02 21:41:29 +0300

    Вывод изображений на экран с использованием Looper/Handler

--- app/src/main/java/com/bignerdranch/android/photogallery/PhotoGalleryFragment.java
+++ app/src/main/java/com/bignerdranch/android/photogallery/PhotoGalleryFragment.java
@@ -1,8 +1,11 @@
 package com.bignerdranch.android.photogallery;
 
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.GridLayoutManager;
@@ -43,7 +46,18 @@ public class PhotoGalleryFragment extends Fragment {
         setRetainInstance(true);
         new FetchItemsTask().execute();
 
-        mThumbnailDownloader = new ThumbnailDownloader<>();
+        Handler responseHandler = new Handler();
+        mThumbnailDownloader = new ThumbnailDownloader<>(responseHandler);
+        mThumbnailDownloader.setThumbnailDownloadListener(
+                new ThumbnailDownloader.ThumbnailDownloadListener<PhotoHolder>(){
+                    @Override
+                    public void onThumbnailDownloaded(PhotoHolder photoHolder, Bitmap bitmap) {
+                        Drawable drawable = new BitmapDrawable(getResources(),bitmap);
+                        photoHolder.bindDrawable(drawable);
+                    }
+                }
+        );
+
         mThumbnailDownloader.start();
         mThumbnailDownloader.getLooper();
         Log.i(TAG, "Background thread started");
@@ -106,6 +120,12 @@ public class PhotoGalleryFragment extends Fragment {
         }
 
 
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        mThumbnailDownloader.clearQueue();
     }
 
     private class FetchItemsTask extends AsyncTask<Void,Void,List<GalleryItem>>{
